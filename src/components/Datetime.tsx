9c254839e44ee3bd17a6938d5941714955a33ca9
张居富 <zhangjufu@192.168.10.13>
2024-03-29 00:41:36 +0800

    feat: init project

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -30,10 +30,10 @@ export default function Datetime({
       </svg>
       {modDatetime && modDatetime > pubDatetime ? (
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          上次修改:
         </span>
       ) : (
-        <span className="sr-only">Published:</span>
+        <span className="sr-only">发布日期:</span>
       )}
       <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <FormattedDatetime
